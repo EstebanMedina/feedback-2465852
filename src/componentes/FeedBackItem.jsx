035ee508216c0c5129efa1c285eefb2a3d434aef
Your Name <you@example.com>
2022-11-17 10:43:39 -0500

    Proyecto creado

---
+++ src/componentes/FeedBackItem.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+const FeedbackItem = () => {
+    //Estados: presisten información a traves del ciclo de vida 
+    //de un unico componente
+    
+    //estados iniciales
+    const [rating, setRating] = useState(2)
+    const [text, setText] = useState("Mejorar Curso, mejores recursos")
+
+     const addNota=()=>{
+        //cambiar estado a rating 
+        //parametro para el valor anterior 
+        setRating((prev)=>{
+            return prev +1
+        })
+     }
+     const restarNota=()=>{
+        //cambiar estado a rating 
+        //parametro para el valor anterior 
+        setRating((prev)=>{
+            return prev -1
+        })
+    }
+
+    return (
+        <div className="card">
+            <div className="num-display">
+                {  rating }
+            </div>
+            <div className="text-display">
+                {text}
+            </div>
+            <button onClick={addNota}>
+                Sumar nota
+            </button>
+            <button onClick={restarNota}>
+                Disminuir nota
+            </button>
+        </div>
+    )
+}
+
+
+export default FeedbackItem
